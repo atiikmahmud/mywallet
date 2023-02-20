b6edaf7ab31e76503c12c31065af7f210a654519
atiikmahmud <atikmahmud.cse@gmail.com>
2023-02-20 17:39:06 +0600

    Home Page Done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +129,53 @@
             </div>
         </div>
     </body>
+</html> --}}
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}">
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+</head>
+
+<body>
+    <div class="welcome-page-area">
+        <div class="container welcome-container">
+            <div class="row d-flex justify-content-center align-items-center welcome-row">
+                <div class="col-md-5">
+                    <div class="card shadow wallet-card-area">
+                        <div class="card-body mt-5 mb-5">
+                            <div class="wallet-logo">
+                                <img src="{{ asset('img/logo.png') }}" alt="My Wallet">
+                            </div>
+                            <div class="wallet-title text-center mt-5">
+                                My Wallet
+                            </div>
+                            <div class="wallet-slug text-center">
+                                Never spend your money before you have it..
+                            </div>
+                            <div class="register-btn text-center mt-5">
+                                <a href="{{ route('register') }}">Register</a>
+                            </div>
+
+                            <div class="login-btn text-center mt-4">
+                                <a href="{{ route('login') }}">Login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
 </html>
