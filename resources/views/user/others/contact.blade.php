2c6b6d2030b76862c87755aae94bcc16295212b9
atiikmahmud <atikmahmud.cse@gmail.com>
2023-02-24 22:17:16 +0600

    Contact Page Design Done

--- resources/views/user/others/contact.blade.php
+++ resources/views/user/others/contact.blade.php
@@ -19,7 +19,53 @@
                                         Contact
                                     </div>
                                     <div class="card-body income-body-area">
-                                        
+                                        <div class="contact-area">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="contact-image mt-4">
+                                                        <img src="{{ asset('img/Contact-Us.jpg') }}" alt="contact-image"
+                                                            height="400px">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="contact-form mt-4">
+                                                        <div class="card">
+                                                            <div class="card-body">
+                                                                <form action="" method="POST">
+                                                                    @csrf
+                                                                    <div class="mb-3">
+                                                                        <label for="name"
+                                                                            class="form-label">Name</label>
+                                                                        <input type="text" class="form-control"
+                                                                            id="name" name="name"
+                                                                            value="Atik Mahmud">
+                                                                    </div>
+
+                                                                    <div class="mb-3">
+                                                                        <label for="email" class="form-label">Email
+                                                                            address</label>
+                                                                        <input type="email" class="form-control"
+                                                                            id="email" name="email"
+                                                                            value="atiikmahmud@gmail.com">
+                                                                    </div>
+
+                                                                    <div class="mb-3">
+                                                                        <label for="message"
+                                                                            class="form-label">Message</label>
+                                                                        <textarea class="form-control"id="message" name="message" rows="5">
+                                                                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                                                                        </textarea>
+                                                                    </div>
+
+                                                                    <button type="submit"
+                                                                        class="btn btn-primary">Send</button>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
