b5cfae6c436c9ce412206e7151ca33dde2a85b4e
atiikmahmud <atikmahmud.cse@gmail.com>
2023-02-25 11:50:36 +0600

    About My Wallet Done

--- resources/views/user/others/about.blade.php
+++ resources/views/user/others/about.blade.php
@@ -19,7 +19,30 @@
                                         About
                                     </div>
                                     <div class="card-body income-body-area">
-                                        
+                                        <div class="contact-area">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="contact-image mt-4">
+                                                        <img src="{{ asset('img/about-wallet.png') }}" alt="contact-image"
+                                                            height="450px">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="contact-form mt-4">
+                                                        <div class="card">
+                                                            <div class="card-body">
+                                                                <div class="about-my-wallet">
+                                                                    <h2 class="text-center">My Wallet</h2>
+                                                                </div>
+                                                                <p style="text-align:justify;">
+                                                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, pariatur porro unde nulla repellendus blanditiis atque illum fuga. Excepturi, suscipit sunt! Atque commodi magnam quae corrupti? Nulla enim quia, expedita esse, deleniti eos totam vero quidem, assumenda fugiat saepe corporis. Nesciunt quam amet minima omnis beatae vero ad repellendus fugiat. Placeat ea non error fugiat voluptate vel illum dolore. Quisquam, similique dicta suscipit, ab voluptatum amet veniam minus eum voluptate id sapiente ut? Dolores officiis deserunt ex cumque nam? Inventore eveniet, exercitationem odio unde aliquid officiis est ex provident praesentium nostrum dolores fuga ipsam maxime dicta et animi omnis sed?
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
